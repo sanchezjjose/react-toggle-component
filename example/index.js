12e4260a751a06a39e2aa8f266e79ec60a814184
josanche <jose.sanchez@mlb.com>
2016-01-09 11:25:11 -0500

    more examples

--- example/index.js
+++ example/index.js
@@ -24,6 +24,7 @@
   const domElement2 = document.getElementById('react-toggle-component-2');
   const domElement3 = document.getElementById('react-toggle-component-3');
   const domElement4 = document.getElementById('react-toggle-component-4');
+  const domElement5 = document.getElementById('react-toggle-component-5');
 
   const httpRequests = {
 
@@ -40,10 +41,10 @@
   
   const opts1 = {
     onState: {
-      text: 'In'
+      text: 'On'
     },
     offState: { 
-      text: 'Out'
+      text: 'Off'
     }
   };
 
@@ -51,7 +52,7 @@
     onState: {
       text: 'In',
       styles: {
-        buttonComponent: { backgroundColor: 'red' },
+        buttonComponent: { backgroundColor: '#d2112e' },
         textComponent: { color: '#fff' },
         sliderComponent: { backgroundColor: '#fff'}
       }
@@ -67,10 +68,46 @@
     }
   };
 
+  const opts3 = {
+    onState: {
+      styles: {
+        buttonComponent: {
+          backgroundColor: '#38677e',
+          width: '80'
+        },
+        sliderComponent: { 
+          height: '30',
+          left: '5',
+          top: '4',
+          transform: 'translate(40px, 0px)',
+          transition: 'transform 0.1s ease',
+          width: '30'
+        }
+      }
+    },
+
+    offState: { 
+      styles: {
+        buttonComponent: {  
+          backgroundColor: '#bbb',
+          width: '80'
+        },
+        sliderComponent: { 
+          height: '30',
+          left: '3',
+          top: '4',
+          transition: 'transform 0.1s ease',
+          width: '30'
+        }
+      }
+    }
+  };
+
   ReactToggleComponent.init(domElement1, true, callback, httpRequests);
   ReactToggleComponent.init(domElement2, false, callback, httpRequests);
   ReactToggleComponent.init(domElement3, true, callback, httpRequests, opts1);
   ReactToggleComponent.init(domElement4, true, callback, httpRequests, opts2);
+  ReactToggleComponent.init(domElement5, true, callback, httpRequests, opts3);
 
   return {};
     
