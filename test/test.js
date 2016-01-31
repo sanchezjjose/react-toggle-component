eeff1dfe4ec3ef20ad4e3c2188a01b2fce7b0459
josanche <jose.sanchez@mlb.com>
2016-01-31 18:15:34 -0500

    travis and tests

---
+++ test/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var expect = require('chai').expect;
+// var toggleButton = require('../lib/index');
+
+describe('#test', function() {
+    it('should pass', function() {
+        expect(true).to.equal(true);
+    });
+});
