41a2d64d58fd918650353f28af6e30b15c82dc58
josanche <jose.sanchez@mlb.com>
2016-02-08 23:42:23 -0500

    refactoring styles into separate prototype object

--- src/components/toggle.jsx
+++ src/components/toggle.jsx
@@ -8,6 +8,7 @@
       './slider', 
       './text', 
       '../utils/http',
+      '../styles/styles',
       'jquery'], 
       factory
     );
@@ -20,6 +21,7 @@
       require('./slider'),
       require('./text'),
       require('../utils/http'),
+      require('../styles/styles'),
       require('jquery')
     );
 
@@ -31,99 +33,84 @@
       root.SliderComponent, 
       root.TextComponent, 
       root.HttpUtil,
+      root.ReactToggleComponentStyles,
       root.jQuery
     );
   }
 
 // background: http://stackoverflow.com/questions/3277182/how-to-get-the-global-object-in-javascript
-}(Function("return this")(), function (React, ReactCSSTransitionGroup, ReactDOM, SliderComponent, TextComponent, HttpUtil, $) {
+}(Function("return this")(), function (React, ReactCSSTransitionGroup, ReactDOM, SliderComponent, TextComponent, HttpUtil, ReactToggleComponentStyles, $) {
+
+  const ToggleButtonComponent = React.createClass({
+
+    getInitialState: function() {
+
+      return { 
+        isActive: this.props.isActive
+      };
+    },
+
+    handleClick: function(elem) {
+
+      if (this.state.isActive) {
+        HttpUtil.POST(this.props.httpRequests.onState.url, this.props.httpRequests.onState.postData, this.props.callback);
+
+      } else {
+        HttpUtil.POST(this.props.httpRequests.offState.url, this.props.httpRequests.offState.postData, this.props.callback);
+      }
+
+      this.setState({ isActive: !this.state.isActive });
+    },
+
+    render: function() {
+
+      const styles = new ReactToggleComponentStyles().toggleComponent();
+
+      if (this.props.opts && this.props.opts.common && this.props.opts.common.styles) {
+        Object.assign(styles.common, this.props.opts.common.styles.buttonComponent);
+      }
+
+      if (this.props.opts && this.props.opts.onState && this.props.opts.onState.styles) {
+        Object.assign(styles.onState, this.props.opts.onState.styles.buttonComponent);
+      }
+
+      if (this.props.opts && this.props.opts.offState && this.props.opts.offState.styles) {
+        Object.assign(styles.offState, this.props.opts.offState.styles.buttonComponent);
+      }
+
+      const toggleButtonStyles = Object.assign({}, styles.offState, this.state.isActive && styles.onState, styles.common);
+
+      return (
+        <div onClick={this.handleClick} style={toggleButtonStyles}>
+          <SliderComponent isActive={this.state.isActive} opts={this.props.opts} />
+          <TextComponent isActive={this.state.isActive} opts={this.props.opts} />
+        </div>
+      );
+    }
+  });
 
   function init(domElement, isActive, callback, httpRequests, opts) {
 
-    function getStyles() {
-
-      let styles = {
-
-        common: {
-          border: '1px solid #ddd',
-          borderRadius: '50px',
-          cursor: 'pointer',
-          display: 'inline-block',
-          fontFamily: '"Helvetica Neue","Helvetica",arial,sans-serif',
-          height: '40px',
-          position: 'relative',
-          textTransform: 'uppercase',
-          width: '100px'
-        },
-
-        offState: {
-          backgroundColor: '#555'
-        },
-
-        onState: {
-          backgroundColor: '#28c891'
-        }
-      };
-
-      return styles;
-    }
-
-    const ToggleButtonComponent = React.createClass({
-
-      getInitialState: function() {
-
-        return { 
-          isActive: isActive
-        };
-      },
-
-      handleClick: function(elem) {
-
-        if (this.state.isActive) {
-          HttpUtil.POST(httpRequests.onState.url, httpRequests.onState.postData, callback);
-
-        } else {
-          HttpUtil.POST(httpRequests.offState.url, httpRequests.offState.postData, callback);
-        }
-
-        this.setState({ isActive: !this.state.isActive });
-      },
-
-      render: function() {
-
-        const styles = getStyles();
-
-        if (this.props.opts && this.props.opts.common && this.props.opts.common.styles) {
-          Object.assign(styles.common, this.props.opts.common.styles.buttonComponent);
-        }
-
-        if (this.props.opts && this.props.opts.onState && this.props.opts.onState.styles) {
-          Object.assign(styles.onState, this.props.opts.onState.styles.buttonComponent);
-        }
-
-        if (this.props.opts && this.props.opts.offState && this.props.opts.offState.styles) {
-          Object.assign(styles.offState, this.props.opts.offState.styles.buttonComponent);
-        }
-
-        const toggleButtonStyles = Object.assign({}, styles.offState, this.state.isActive && styles.onState, styles.common);
-
-        return (
-          <div onClick={this.handleClick} style={toggleButtonStyles}>
-            <SliderComponent isActive={this.state.isActive} opts={this.props.opts} />
-            <TextComponent isActive={this.state.isActive} opts={this.props.opts} />
-          </div>
-        );
-      }
-    });
+    console.log(ToggleButtonComponent);
 
     ReactDOM.render(
-      <ToggleButtonComponent opts={opts} />,
+
+      <ToggleButtonComponent 
+        isActive={isActive} 
+        callback={callback} 
+        httpRequests={httpRequests} 
+        opts={opts} />,
+
       domElement
     );
   }
 
   return {
-    init : init
+    init : init,
+
+    getComponent: function() {
+      return ToggleButtonComponent;
+    }
   };
 
 }));
--- test/test.js
+++ test/test.js
@@ -1,21 +1,50 @@
 import React from 'react';
 import {createRenderer} from 'react-addons-test-utils';
-import ToggleButtonComponent from '../lib/components/toggle.js';
+import Component from '../lib/components/toggle.js';
 
 const expect = require('chai').expect;
 
+const domElement = <div class="toggle-button" id="react-toggle-component-3"></div>;
+
+const isActive = true;
+
+const httpRequests = {
+  onState: {
+    url: 'https://randomuser.me/api',
+    postData: {}
+  },
+
+  offState: {
+    url: 'https://randomuser.me/api',
+    postData: {}
+  }
+};
+
+function callback(data) {
+  console.log(data);
+}
 
 describe('#test', function() {
     it('should pass', function() {
+
+      let ToggleButtonComponent = Component.getComponent();
       let renderer = createRenderer();
       let opts = {};
 
-      console.log(renderer);
+      console.log(ToggleButtonComponent);
       
-      renderer.render(<ToggleButtonComponent opts={opts}/>);
+      var actualRender = renderer.render(
+        <ToggleButtonComponent 
+          isActive={isActive} 
+          callback={callback} 
+          httpRequests={httpRequests} 
+          opts={opts} />
+      );
 
-      let actualElement = renderer.getRenderOutput();
-      console.log(actualElement);
+      console.log(actualRender);
+
+      // let actualElement = renderer.getRenderOutput();
+      // console.log(actualElement);
 
       expect(true).to.equal(true);
     });
